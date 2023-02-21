4bf34516cd5d33bfe8ecb25c7a75cde7441b403a
Jerry M Yang <z5421983@ad.unsw.edu.au>
2023-02-21 17:50:35 +1100

    Added function stub for channelDetailsV1 as well as heading comment for channel.js

---
+++ src/channel.js
@@ -0,0 +1,32 @@
+/**
+ * channel.js
+ * 
+ * Contains the stub code functions of all channel* functions.
+ */
+
+
+function channelDetailsV1(authUserId, channelId) {
+  return {
+    name: 'Hayden',
+    ownerMembers: [
+      {
+        uId: 1,
+        email: 'example@gmail.com',
+        nameFirst: 'Hayden',
+        nameLast: 'Jacobs',
+        handleStr: 'haydenjacobs',
+      }
+    ],
+    allMembers: [
+      {
+        uId: 1,
+        email: 'example@gmail.com',
+        nameFirst: 'Hayden',
+        nameLast: 'Jacobs',
+        handleStr: 'haydenjacobs',
+      }
+    ],
+  }
+}
+
+    
