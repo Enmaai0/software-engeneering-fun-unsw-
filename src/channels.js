8496388cb0d4dd3e318ea7780c45c667b3fa553a
Rishabh Patel <z5421642@ad.unsw.edu.au>
2023-02-23 10:43:08 +1100

    added channels

--- src/channels.js
+++ src/channels.js
@@ -1,14 +1,15 @@
 function channelsCreateV1( authUserId, name, isPublic ){
-    return { channelId: 1 };
+  return { channelId: 1 };
 }
 
 function channelsListV1( authUserId ){
-     return {channels: [
-    {
-      channelId: 1,
-      name: 'My Channel',
-    }
-  ],
+  return {
+    channels: [
+      {
+        channelId: 1,
+        name: 'My Channel',
+      }
+    ],
   }
 }
 
