44b9431bb58e06915aacf96e80f0cc1378e238ab
Xingyu Liu <z5408279@ad.unsw.edu.au>
2023-02-23 10:45:28 +1100

    auth.js stub function updated

---
+++ src/auth.js
@@ -0,0 +1,13 @@
+
+
+function authLoginV1(email, password) {
+  return {
+    authUserId: 1
+  };
+}
+
+function authRegisterV1(email, password, nameFirst, nameLast) {
+  return {
+    authUserId: 1
+  };
+}
