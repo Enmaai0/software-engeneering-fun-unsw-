c02b4036e1d44642a5c75f99f5c56a1797a49e66
Rishabh Patel <z5421642@ad.unsw.edu.au>
2023-03-08 13:08:04 +1100

    functionEdit(ChannelsCreateV1),channels.js, wrote the create functions for channels.

--- src/channels.js
+++ src/channels.js
@@ -4,10 +4,41 @@
  * Contains the stub code functions of all channels* functions.
  */
 
+
+//Creates a new channel with the given name, that is either a public or private channel.
+//The user who created it automatically joins the channel.
 function channelsCreateV1(authUserId, name, isPublic) {
-  return { 
-    channelId: 1
-  };
+// Check if the channel name is valid
+  if (name.length < 1 || name.length > 20) {
+    return { error:'Name is too short.'};
+    }
+
+  // Check if the authUserId is valid
+  if (typeof authUserId !== 'string' || authUserId.trim().length === 0) {
+    return { error: 'error' };
+  }
+  
+  for (let user of dataStore.users) {
+    if (user.authUserId === authUserId) {
+        let channelobj = {
+          name,
+          isPublic,
+          owner: [authUserId],
+          allMembers: [authUserId],
+          messages: [],
+          channelId: dataStore.channels.length,
+        }
+
+        const channelId = dataStore.channels.length;
+
+        dataStore.channels.push(channelobj);
+        setData(dataStore);
+
+        return {channelId : channelId};
+    }
+
+  }
+  return {error : 'User is not valid'}
 }
 
 function channelsListAllV1(authUserId) { 
