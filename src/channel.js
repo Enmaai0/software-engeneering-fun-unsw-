7af21fd33cc51609a288c003bd10e6fbb6003a7b
Hao Zhang <z5440607@ad.unsw.edu.au>
2023-03-09 19:43:38 +1100

    functionEdit/channelJoinV1/channel.js/channelJoinV1 completed

--- src/channel.js
+++ src/channel.js
@@ -5,18 +5,18 @@
  */
 import { getData } from "./dataStore"
 
-export function channelDetailsV1(authUserId, channelId) {
+function channelDetailsV1(authUserId, channelId) {
   //error checking
-  let channel = {};
+  let data = getData();
   
-  if (is_Valid_userId(authUserId, channelId) === false || is_Valid_ChannelId(authUserId, channelId) === false) {
+  if (is_Valid_userId(authUserId) === false || is_Valid_ChannelId(channelId) === false) {
     return {error: 'Id error'};
   }
 
   //function
-  for (const i of data.channels) {
-    if (i.channelId === channelId) {
-      return i;
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      return channel;
     }
   }
 }
@@ -45,13 +45,41 @@ export function channelDetailsV1(authUserId, channelId) {
 }
 */
 function channelJoinV1(authUserId, channelId) {
+  let data = getData();
+  //error checking
+  if (is_Valid_userId(authUserId) === false || is_Valid_ChannelId(channelId) === false) {
+    return {error: 'Id error'};
+  }
+
+  //check whether is already a member
+  if (check_allMembers(authUserId, channelId) === false) {
+    return {error: 'already a member'};
+  }
+  //fail to join a private channel
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId && i.is_Public === false) {
+      return {error: 'no permission to join the channel'};
+    }
+  }
+  //start to join
+  let the_channel = {};
+  let the_user = {};
+  for (const user of data.users) {
+    if (user.authUserId === authUserId) {
+      the_user = user;
+    }
+  }
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      the_channel = channel;
+    }
+  }
+  the_channel.allMembers.push(the_user);
 
 }
-
 function channelInviteV1(authUserId, channelId, uId) {
   return {};
 }
-
 function channelMessagesV1(authUserId, channelId, start) {
   return {
     messages: [
@@ -67,7 +95,7 @@ function channelMessagesV1(authUserId, channelId, start) {
   };
 }
     
-function  is_Valid_userId(authUserId, channelId) {
+function  is_Valid_userId(authUserId) {
   let valid_userId = false;
   const data = getData();
 
@@ -83,7 +111,7 @@ function  is_Valid_userId(authUserId, channelId) {
   }
 }
 
-function is_Valid_ChannelId(authUserId, channelId) {
+function is_Valid_ChannelId(channelId) {
   let valid_channelId = false;
   const data = getData();
 
