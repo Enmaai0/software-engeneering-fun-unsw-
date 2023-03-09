bc876c530a34f0bc39a0be239e1d64d1e0ea88d2
Xingyu Liu <z5408279@ad.unsw.edu.au>
2023-03-09 12:06:29 +1100

    functionEdit/channelInviteV1/channel.js/implemented channelInviteV1 function and export functions

--- src/channel.js
+++ src/channel.js
@@ -4,6 +4,8 @@
  * Contains the stub code functions of all channel* functions.
  */
 
+import { getData } from "./dataStore"
+
 function channelDetailsV1(authUserId, channelId) {
   return {
     name: 'Hayden',
@@ -33,6 +35,44 @@ function channelJoinV1(authUserId, channelId) {
 }
 
 function channelInviteV1(authUserId, channelId, uId) {
+  const data = getData();
+
+  if (channelId <= data.channels.length) {
+    return {error: 'Invalid channelId'};
+  }
+
+  if (authUserId <= data.authUserId.length) {
+    return {error: 'Invalid authUserId'};
+  }
+
+  if (uId <= data.users.length) {
+    return {error: 'Invalid uId'};
+  }
+
+  let check_authUserId = false;
+  for (const a of data.channels[channelId].allMembers) {
+    if (a.authUserId === authUserId) {
+      check_authUserId = true;
+    }
+  }
+
+  if (!check_authUserId) {
+    return  {error: 'authUserId does not have permission'};
+  }
+
+  let check_uId = false;
+  for (const a of data.channels[channelId].allMembers) {
+    if (a.authUserId === authUserId) {
+      check_uId = true;
+    }
+  }
+
+  if (check_uId) {
+    return  {error: 'user already in channel'};
+  }
+
+  data.channels[channelId].allMembers.push(uId);
+
   return {};
 }
 
@@ -51,3 +91,4 @@ function channelMessagesV1(authUserId, channelId, start) {
   };
 }
     
+export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 }
