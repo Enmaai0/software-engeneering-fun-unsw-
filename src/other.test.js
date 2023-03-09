3a7ed9081940804806558a38290d7d539ce85d7b
Hao Zhang <z5440607@ad.unsw.edu.au>
2023-03-09 11:01:47 +1100

    testEdit/clearV1/other.test.js/clearV1 test completed

--- src/other.test.js
+++ src/other.test.js
@@ -5,4 +5,18 @@
  * not included in the main function files
  */
 
-import { {{INSERT FUNCTIONS IMPORTED HERE}} } from './other.js';
+import { authLoginV1, authRegisterV1 } from './auth.js';
+import { clearV1 } from './other.js';
+import { userProfileV1 } from "./users.js"
+
+
+////////////// clearV1 Function ///////////////
+describe('reset data to initial state',() => {
+    let user = authRegisterV1('userEmail@gmail.com', 'password', 'First', 'Last');
+    user = authLoginV1('userEmail@gmail.com', 'password');
+    channel = channelsCreateV1(user.authUserId, 'firstChannel', true);
+    clearV1();
+    test('everything is now deleted', () => {
+        expect(userProfileV1(user.authUserId, user.authUserId)).toStrictEqual({error: expect.any(String)});
+    })
+})
