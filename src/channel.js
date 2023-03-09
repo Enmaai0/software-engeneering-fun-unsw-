1a4ac6915ed11cfc2c984720c7973afdd95aafb3
Hao Zhang <z5440607@ad.unsw.edu.au>
2023-03-09 19:47:39 +1100

    testEdit/clearV1/other.test.js/clearV1 test completed

--- src/channel.js
+++ src/channel.js
@@ -3,8 +3,24 @@
  * 
  * Contains the stub code functions of all channel* functions.
  */
+import { getData } from "./dataStore"
 
 function channelDetailsV1(authUserId, channelId) {
+  //error checking
+  let data = getData();
+  
+  if (is_Valid_userId(authUserId) === false || is_Valid_ChannelId(channelId) === false) {
+    return {error: 'Id error'};
+  }
+
+  //function
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      return channel;
+    }
+  }
+}
+/*
   return {
     name: 'Hayden',
     ownerMembers: [
@@ -27,15 +43,43 @@ function channelDetailsV1(authUserId, channelId) {
     ],
   };
 }
-
+*/
 function channelJoinV1(authUserId, channelId) {
-  return {};
-}
+  let data = getData();
+  //error checking
+  if (is_Valid_userId(authUserId) === false || is_Valid_ChannelId(channelId) === false) {
+    return {error: 'Id error'};
+  }
 
+  //check whether is already a member
+  if (check_allMembers(authUserId, channelId) === false) {
+    return {error: 'already a member'};
+  }
+  //fail to join a private channel
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId && i.is_Public === false) {
+      return {error: 'no permission to join the channel'};
+    }
+  }
+  //start to join
+  let the_channel = {};
+  let the_user = {};
+  for (const user of data.users) {
+    if (user.authUserId === authUserId) {
+      the_user = user;
+    }
+  }
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      the_channel = channel;
+    }
+  }
+  the_channel.allMembers.push(the_user);
+
+}
 function channelInviteV1(authUserId, channelId, uId) {
   return {};
 }
-
 function channelMessagesV1(authUserId, channelId, start) {
   return {
     messages: [
@@ -50,4 +94,36 @@ function channelMessagesV1(authUserId, channelId, start) {
     end: 50,
   };
 }
+    
+function  is_Valid_userId(authUserId) {
+  let valid_userId = false;
+  const data = getData();
+
+  for (const i of data.users) {
+    if (i.authUserId === authUserId) {
+      valid_userId = true;
+    }
+  }
+  if (!valid_userId) {
+    return false;
+  }else {
+    return true;
+  }
+}
+
+function is_Valid_ChannelId(channelId) {
+  let valid_channelId = false;
+  const data = getData();
+
+  for (const i of data.channels) {
+    if (i.channelId === channelId) {
+      valid_channelId = true;
+    }
+  }
+  if (!valid_channelId) {
+    return false;
+  }else {
+    return true;
+  }
+}_
     
