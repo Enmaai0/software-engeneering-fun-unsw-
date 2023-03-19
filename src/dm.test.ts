0aad170d851db4e48f08cc30f0d983e2118424ae
Jerry M Yang <z5421983@ad.unsw.edu.au>
2023-03-19 16:45:39 +1100

    functionEdit and testEdit/dmCreate & dmCreateTests/dm.ts & dm.tests.ts/Implemented tests and functionality for /dc/create/v1. Some errors still contained due to lack of implementation of other aspects. Added stub functions for all other dm* routes

---
+++ src/dm.test.ts
@@ -0,0 +1,150 @@
+/**
+ * dm.test.ts
+ * Contains the jest testing designed for dm.ts that utilises 
+ * the HTTP routes created to test funcionality
+ */
+
+import request from 'sync-request';
+import config from './config.json';
+import { testClear } from './other.test';
+import { testAuthRegister } from './auth.test';
+
+const OK = 200;
+const port = config.port;
+const url = config.url;
+
+const ERROR = { error: expect.any(String) };
+
+interface AuthRegisterReturn {
+  token: string;
+  authUserId: number;
+}
+
+interface DmId {
+  dmId: number;
+}
+
+interface DmDetailsReturn {
+  name: string;
+  members: Member[];
+}
+
+interface Member {
+  uId: number;
+  email: string;
+  nameFirst: string;
+  nameLast: string;
+  handleStr: string;
+}
+
+beforeEach(() => {
+  testClear();
+});
+
+/** /dm/create/v1 Testing **/
+
+function testDmCreate(token: string, uIds: number[]) {
+  const res = request(
+    'POST',
+    `${url}:${port}/dm/create/v1`,
+    {
+      json: {
+        token,
+        uIds
+      }
+    }
+  );
+  return JSON.parse(res.getBody() as string);
+}
+
+describe('/dm/create: Error Testing', () => {
+  let testUser1: AuthRegisterReturn; 
+  let testUser2: AuthRegisterReturn;
+  let testUser3: AuthRegisterReturn;
+  beforeEach(() => {
+    testUser1 = testAuthRegister('email@gmail.com', 'pass1234', 'Test', 'Bot');
+    testUser2 = testAuthRegister('secondemail@gmail.com', 'pass1234', 'Test', 'Bot II');
+  });
+
+  test('Token: Invalid Token', () => {
+    expect(testDmCreate(testUser1.token + 'a', [testUser2.authUserId])).toStrictEqual(ERROR);
+  });
+
+  test('uIds: Duplicate uIds', () => {
+    testUser3 = testAuthRegister('thirdemail@gmail.com', 'pass1234', 'Test', 'Bot III');
+    expect(testDmCreate(testUser1.token, [testUser2.authUserId, testUser3.authUserId, testUser2.authUserId])).toStrictEqual(ERROR);
+  });
+
+  test('uIds: Invalid uIds (Only Invalid)', () => {
+    expect(testDmCreate(testUser1.token, [testUser2.authUserId + 1])).toStrictEqual(ERROR);
+  });
+
+  test('uIds: Invalid uIds (Mixed Invalid)', () => {
+    testUser3 = testAuthRegister('thirdemail@gmail.com', 'pass1234', 'Test', 'Bot III');
+    expect(testDmCreate(testUser1.token, [testUser3.authUserId, testUser2.authUserId + 1])).toStrictEqual(ERROR);
+  });
+});
+
+describe('/dm/create: Return Testing', () => {
+  let testUser1: AuthRegisterReturn; 
+  let testUser2: AuthRegisterReturn;
+  let testUser3: AuthRegisterReturn;
+  beforeEach(() => {
+    testUser1 = testAuthRegister('email@gmail.com', 'pass1234', 'Test', 'Bot');
+    testUser2 = testAuthRegister('secondemail@gmail.com', 'pass1234', 'Test', 'Bot II');
+    testUser3 = testAuthRegister('thirdemail@gmail.com', 'pass1234', 'Test', 'Bot III');
+  });
+
+  test('Correct Return: 1 to 1 Dm', () => {
+    expect(testDmCreate(testUser1.token, [testUser2.authUserId])).toStrictEqual({ dmId: expect.any(Number) });
+  });
+
+  test('Correct Return: 3 Person Dm', () => {
+    expect(testDmCreate(testUser1.token, [testUser2.authUserId, testUser3.authUserId])).toStrictEqual({ dmId: expect.any(Number) });
+  });
+
+  test('Correct Return: Someone Else Making Dm', () => {
+    expect(testDmCreate(testUser2.token, [testUser3.authUserId, testUser1.authUserId])).toStrictEqual({ dmId: expect.any(Number) });
+  });
+});
+
+describe('/dm/create: dmName Testing', () => {
+  let testUser1: AuthRegisterReturn; 
+  let testUser2: AuthRegisterReturn;
+  let testUser3: AuthRegisterReturn;
+  let testDm: DmId;
+  let testDmDetail: DmDetailsReturn;
+
+  test('Correct Return: Just Owner', () => {
+    testUser1 = testAuthRegister('LonelyGuy@gmail.com', 'pass1234', 'Lonely', 'Guy');
+    testDm = testDmCreate(testUser1.token, []);
+    testDmDetail = testDmDetails(testUser1.token, testDm.dmId);
+    expect(testDmDetail.name).toStrictEqual('lonelyguy');
+  });
+
+  test('Correct Return: Just Letters', () => {
+    testUser1 = testAuthRegister('a@gmail.com', 'pass1234', 'a', 'a');
+    testUser2 = testAuthRegister('b@gmail.com', 'pass1234', 'a', 'b');
+    testUser3 = testAuthRegister('c@gmail.com', 'pass1234', 'b', 'a');
+    testDm = testDmCreate(testUser1.token, [testUser3.authUserId, testUser2.authUserId]);
+    testDmDetail = testDmDetails(testUser1.token, testDm.dmId);
+    expect(testDmDetail.name).toStrictEqual('aa, ab, ba');
+  });
+
+  test('Correct Return: Longer Names', () => {
+    testUser1 = testAuthRegister('email@gmail.com', 'pass1234', 'Test', 'Bot');
+    testUser2 = testAuthRegister('secondemail@gmail.com', 'pass1234', 'Test', 'Bot II');
+    testUser3 = testAuthRegister('thirdemail@gmail.com', 'pass1234', 'Test', 'Bot III');
+    testDm = testDmCreate(testUser1.token, [testUser3.authUserId, testUser2.authUserId]);
+    testDmDetail = testDmDetails(testUser1.token, testDm.dmId);
+    expect(testDmDetail.name).toStrictEqual('testbot, testbotii, testbotiii');
+  });
+});
+
+/** /dm/list/v1 Testing **/
+
+/** /dm/details/v1 Testing **/
+
+/** /dm/leave/v1 Testing **/
+
+/** /dm/messages/v1 Testing **/
---
+++ src/dm.ts
@@ -0,0 +1,290 @@
+/**
+ * dm.ts
+ *
+ * Contains the functions of all dm* functions.
+ */
+
+import { getData, setData } from './dataStore';
+
+interface Error {
+  error: string;
+}
+
+interface DmId {
+  dmId: number;
+}
+
+interface DmDataStoreObject {
+  dmId: number,
+  name: string,
+  owner: number,
+  members: number[],
+  messages: Message[],
+}
+
+interface DmObject {
+  dmId: number;
+  name: string;
+}
+
+interface Dms {
+  dms: DmObject[];
+}
+
+interface Member {
+  uId: number;
+  email: string;
+  nameFirst: string;
+  nameLast: string;
+  handleStr: string;
+}
+
+interface DmDetails {
+  name: string;
+  members: Member[];
+}
+
+interface Message {
+  messageId: number;
+  uId: number;
+  message: string;
+  timeSent: number;
+}
+
+interface DmMessages {
+  messages: Message[];
+  start: number;
+  end: number;
+}
+
+/**
+ * dmCreate
+ *
+ * Creates a new Id between the creator and all uIds
+ * listed in the array passed through.
+ *
+ * @param {string} token
+ * @param {number[]} uIds 
+ * @returns {{ DmId: number }}
+ */
+function dmCreate(token: string, uIds: number[]): DmId | Error {
+  if (!isValidToken(token)) {
+    return { error: 'Invalid Token'};
+  }
+
+  if (hasDuplicates(uIds)) {
+    return { error: 'uId array cannot contain duplicates' };
+  }
+
+  for (let i = 0; i < uIds.length; i++) {
+    if (!isUserId(uIds[i])) {
+      return { error: 'A user Id given does not exist' };
+    }
+  }
+
+  let data = getData();
+
+  const dmId = data.dms.length as number;
+  const ownerId = getIdFromToken(token);
+  const memberIdandOwnerId = uIds;
+  memberIdandOwnerId.push(ownerId);
+  const dmName = generateDmName(memberIdandOwnerId);
+
+  let dmObject: DmDataStoreObject = {
+    dmId: dmId,
+    name: dmName,
+    owner: ownerId,
+    members: uIds,
+    messages: [],
+  }
+
+  data.dms.push(dmObject);
+
+  return { dmId: dmId };
+}
+
+/**
+ * isValidToken
+ *
+ * Given a token returns whether the token exists
+ * within the dataStore or not.
+ *
+ * @param {string} token 
+ * @returns {boolean}
+ */
+function isValidToken(token: string): boolean {
+  const data = getData();
+
+  for (const user of data.users) {
+    let userTokenArray = data.users.tokens;
+    if (userTokenArray.includes(token)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * isUSerId
+ *
+ * Given a uId checks if that uId exists
+ *
+ * @param {number} id 
+ * @returns {boolean}
+ */
+function isUserId(id: number): boolean {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.uId === id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * hasDuplicates
+ * 
+ * Given an array checks if there a duplicates contained
+ * with in. Utilises set theory to check.
+ * 
+ * @param {array} array 
+ * @returns 
+ */
+function hasDuplicates(array: any[]): boolean {
+  const arraySet = new Set(array);
+  if (array.length !== arraySet.size) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * getIdFromToken
+ *
+ * Given a token extracts the uId of the person
+ * associated with that token. 
+ * Errors should not occur due to previous error test
+ *
+ * @param {string} token
+ * @returns {number}
+ */
+function getIdFromToken(token: string): number {
+  const data = getData();
+
+  for (const user of data.users) {
+    let userTokenArray = data.users.tokens;
+    if (userTokenArray.includes(token)) {
+      return user.uId;
+    }
+  }
+  return -1;
+}
+
+function generateDmName(idArray: number[]): string {
+  const data = getData();
+
+  let userHandleArray = [];
+
+  for (const user of data.users) {
+    if (idArray.includes(user.uId)) {
+      userHandleArray.push(user.userHandle);
+    }
+  }
+  
+  // Sorts the array of all handles alphabetically (see .sort)
+  userHandleArray.sort();
+
+  let dmName = '';
+  for (const handle of userHandleArray) {
+    dmName.concat(handle + ', ');
+  }
+
+  // Removes the last uneeded ', ' from the end of the name
+  dmName = dmName.slice(0,-2);
+
+  return dmName;
+}
+
+/**
+ * dmList
+ *
+ * Given a valid user token, lists of all the Dm's 
+ * that the user is a member of.
+ *
+ * @param {string} token 
+ * @returns { dms: [] }
+ */
+function dmList(token: string): Dms {
+  
+  return { dms: [] };
+}
+
+/**
+ * dmRemove
+ *
+ * Remove an existing DM, so all members are no longer in the DM.
+ * This can only be done by the original creator of the DM.
+ *
+ * @param {string} token 
+ * @param {number} dmId 
+ * @returns {}
+ */
+function dmRemove(token: string, dmId: number): {} {
+
+  return {};
+}
+
+/**
+ * dmDetails
+ *
+ * Given a DM with ID dmId that the authorised user is a member of, 
+ * provide basic details about the DM.
+ *
+ * @param {string} token 
+ * @param {number} dmId 
+ * @returns {{ DmDetails }}
+ */
+function dmDetails(token: string, dmId: number): DmDetails {
+  
+  return {
+    name: 'placeholder',
+    members: []
+  };
+}
+/**
+ * dmLeave
+ *
+ * Given a DM ID, the user is removed as a member of this DM. 
+ * The creator is allowed to leave and the DM will still exist
+ * if this happens. This does not update the name of the DM.
+ *
+ * @param {string} token 
+ * @param {number} dmId 
+ * @returns {}
+ */
+function dmLeave(token: string, dmId: number): {} {
+  
+  return {};
+}
+
+/**
+ * dmMessages
+ *
+ * Given a user Token, dmId, and start value, returns a 
+ * list of 50 messages from index start to start + 50.
+ *
+ * @param {string} token 
+ * @param {number} dmId 
+ * @param {number} start 
+ * @returns {{ DmMessages }}
+ */
+function dmMessages(token: string, dmId: number, start: number): DmMessages {
+  
+  return {
+    messages: [],
+    start: 0,
+    end: 0
+  }
+}
